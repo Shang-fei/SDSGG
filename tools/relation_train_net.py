19e811f08ad5e26f2ce4df39be8e750c10ac64c7
FEI <cloud_catkin@outlook.com>
2026-06-05 14:47:43 +0800

    同步

--- tools/relation_train_net.py
+++ tools/relation_train_net.py
@@ -204,11 +204,13 @@ def train(cfg, local_rank, distributed, logger):
             checkpointer.save("model_final", **arguments)
 
         val_result = None # used for scheduler updating
-        if cfg.SOLVER.TO_VAL and iteration % cfg.SOLVER.VAL_PERIOD == 0 and iteration>=12000:
+        if cfg.SOLVER.TO_VAL and (iteration+1) % cfg.SOLVER.VAL_PERIOD == 0:
             logger.info("Start validating")
-            #run_test(cfg, model, distributed, logger)
+            test_result = run_test(cfg, model, distributed, logger)
+            logger.info("Test Result: %.4f" % test_result)
             val_result = run_val(cfg, model, val_data_loaders, distributed, logger)
             logger.info("Validation Result: %.4f" % val_result)
+
 
              
         # scheduler should be called after optimizer.step() in pytorch>=1.1.0
@@ -299,8 +301,10 @@ def run_test(cfg, model, distributed, logger):
     if cfg.MODEL.ATTRIBUTE_ON:
         iou_types = iou_types + ("attributes", )
     output_folders = [None] * len(cfg.DATASETS.TEST)
-    
+
     dataset_names = cfg.DATASETS.TEST
+    test_result = []
+
     if cfg.OUTPUT_DIR:
         for idx, dataset_name in enumerate(dataset_names):
             output_folder = os.path.join(cfg.OUTPUT_DIR, "inference", dataset_name)
@@ -308,7 +312,7 @@ def run_test(cfg, model, distributed, logger):
             output_folders[idx] = output_folder
     data_loaders_val = make_data_loader(cfg, mode='test', is_distributed=distributed)
     for output_folder, dataset_name, data_loader_val in zip(output_folders, dataset_names, data_loaders_val):
-        inference(
+        dataset_result = inference(
             cfg,
             model,
             data_loader_val,
@@ -322,7 +326,18 @@ def run_test(cfg, model, distributed, logger):
             logger=logger,
         )
         synchronize()
+        test_result.append(dataset_result)
+    # support for multi gpu distributed testing
+    gathered_result = all_gather(torch.tensor(dataset_result).cpu())
+    gathered_result = [t.view(-1) for t in gathered_result]
+    gathered_result = torch.cat(gathered_result, dim=-1).view(-1)
+    valid_result = gathered_result[gathered_result>=0]
+    test_result = float(valid_result.mean())
+    del gathered_result, valid_result
+    torch.cuda.empty_cache()
+
     model.updata(cfg.OV_SETTING.TRAIN_PART)
+    return test_result
 
 
 def main():
